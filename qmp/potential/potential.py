36796eadf22d6484a834f5e898a3a69ef9579d1c
Reinhard Maurer <reinhard.maurer@yale.edu>
2016-05-04 22:18:28 -0400

    end of day commit, modified class potential

--- qmp/potential/potential.py
+++ qmp/potential/potential.py
@@ -1,5 +1,5 @@
 """
-
+class potential
 """
 
 from qmp.tools.utilities import *
@@ -7,46 +7,111 @@ import numpy as np
 
 class Potential(object):
     """
-    Defines Potential and all operations on it
+    Defines Potential and all operations on it. Functions for the 
+    potential energy, the first and second derivative, and for possible 
+    excited states and scalar and vectorial derivative couplings must 
+    be passed explicitly.
     """
 
-    def __init__(self, cell=None, f=None, firstd=None, secondd=None):
+    def __init__(self, cell=None, f=None, firstd=None, secondd=None, \
+            n=1, d1=None, d2=None ):
         """
-
+        Initializes potential
+        Input
+            cell: domain given as list of two values
+            f:    function that defines potential
+            firstd: first derivative, if not given is calculated numerically
+            secondd: second derivative, if not given is calculated numerically
+            n: number of states, default is 1. if n>1, f,d1,d2,firstd, secondd are lists of functions
+            d1: first order couplings
+            d2: second order couplings
         """
 
         if cell is None:
             cell = [[0.,1.]]
         self.cell = cell
-        self.f = f
-        self.firstd = firstd
-        self.secondd = secondd
+        if n is None:
+            self.n = 1
+        else:
+            self.n = n
+        if isinstance(f,list):
+            self.f = f
+        else:
+            self.f = [f]
+        if isinstance(firstd,list):
+            self.firstd = firstd
+        else:
+            self.firstd = [firstd]
+        if isinstance(secondd,list):
+            self.secondd = secondd
+        else:
+            self.secondd = [secondd]
+        if isinstance(d1,list):
+            self.d1 = d1
+        else:
+            self.d1 = [d1]
+        if isinstance(d2,list):
+            self.d2 = d2
+        else:
+            self.d2 = [d2]
         self.data = None
 
-    def __call__(self,x):
-
-        if self.f is None:
+    def __call__(self,x,n=0):
+        """
+        calculate potential at point x or list of points x
+        """
+        f = self.f[n]
+        if f is None:
             return np.zeros_like(x)
         else:
-            return self.f(x)
+            return f(x)
     
-    def deriv(self,x):
-
-        if self.firstd is None:
+    def deriv(self,x,n=0):
+        """
+        calculate 1st derivative at point x or list of points x
+        """
+        firstd = self.firstd[n]
+        if firstd is None:
             return num_deriv(self.f,x)
         else:
-            return self.firstd(x)
+            return firstd(x)
 
-    def hess(self,x):
-
-        if self.secondd is None:
+    def hess(self,x,n=0):
+        """
+        calculate 2nd derivative at point x or list of points x
+        """
+        secondd = self.secondd[n]
+        if secondd is None:
             return num_deriv2(self.f,x)
         else:
-            return self.secondd(x)
+            return secondd(x)
+
+    #### coupling is different for different PES pairs
+    #def coupling_d1(self,x,n=0, m=1):
+        #"""
+        #calculate nonadiabatic vectorial 1st order coupling
+        #"""
+        #d1 = self.d1[n]
+        #if d1 is None:
+            #return np.zeros_like(x)
+        #else:
+            #return d1(x)
+    
+    #def coupling_d2(self,x,n=0, m=1):
+        #"""
+        #calculate nonadiabatic vectorial 1st order coupling
+        #"""
+        #d2 = self.d2[n,m]
+        #if d2 is None:
+            #return np.zeros_like(x)
+        #else:
+            #return d2(x)
 
 
     def plot_pot(self,start=0.0, end=1.0, pts=50):
-
+        """
+        plot potential with matplotlib
+        """
         try:
             from matplotlib import pyplot as plt
         except:
@@ -61,6 +126,12 @@ class Potential(object):
 
 
 class Potential2D(object):
+    """
+    Defines Potential and all operations on it. Functions for the 
+    potential energy, the first and second derivative, and for possible 
+    excited states and scalar and vectorial derivative couplings must 
+    be passed explicitly.
+    """
 
     def __init__(self, cell=None, f=None, firstd=None, secondd=None):
 
